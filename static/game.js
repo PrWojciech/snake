977fb7481343d816388dbf5983fd27cf58b41a44
KZwolski <kkzwolski@gmail.com>
2022-05-30 14:19:24 +0200

    Timer

--- static/game.js
+++ static/game.js
@@ -1,5 +1,24 @@
 initGame();
 
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
