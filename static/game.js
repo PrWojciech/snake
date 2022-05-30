2c75a7a215aae90ccda7f3b53f6328d69304f2a2
BartoszGrabowski1 <heksargonh@gmail.com>
2022-05-30 14:12:09 +0200

    snake increaseSize but on click now

--- static/game.js
+++ static/game.js
@@ -6,36 +6,10 @@
 //
 // }
 
-document.onkeydown = keyPress
+// document.onkeydown = keyPress
 
-const snakeSpeed = 1
-let snakeBody = [
-    {x: 0, y: 0},
-    {x: 1, y: 0},
-    {x: 2, y: 0}
-
-
-
-]
 let snakeSize = 25
-
 const snake = document.querySelector('.snake')
-
-
-function keyPress(e) {
-    e.preventDefault();
-    e = e || window.event;
-
-    // Game pause on escape btn
-    if (e.keyCode == 27){
-        console.log('Pause')
-    }
-
-    // Game start on enter btn
-    if (e.keyCode == 13){
-        console.log('start')
-    }
-}
 
 
 function increaseSize () {
@@ -44,10 +18,5 @@ function increaseSize () {
     console.log(snakeSize)
 }
 
-function draw() {
-    snakeBody.forEach(segment => {
-        const snakeElement = document.createElement('div')
-    })
-}
 
 snake.addEventListener('click', increaseSize)
